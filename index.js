11da80cf5eec707fd3b89a739e75ab54f2beb701
Kene Arionye <kenechiaugustine@gmail.com>
2022-04-23 20:20:50 +0100

    used res.write

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http  = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Welcome to my HEROKU APP TEST\n');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Hello World</h1>');
+    res.end();
     }
 );
 
