b49485f39d29c49c1b46ae205c7c6b336872e138
Kene Arionye <kenechiaugustine@gmail.com>
2022-04-23 20:26:17 +0100

    routing

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const http  = require('http');
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Hello World</h1>');
-    res.end();
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>Hello, Welcome to my page</h1>');
+
+        res.end();
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>About Page</h1>');
+
+        res.end();
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>404 Error occurred</h1>');
+
+        res.end();
     }
-);
+
+});
 
 const PORT = process.env.PORT || 5000;
 
